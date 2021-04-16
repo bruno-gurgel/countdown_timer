9d337fa38aaa3bf82ee737b559a043288cb25822
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-04-16 17:08:36 -0300

    feat: Added page skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App">
+			<h1 className="heading">We're launching soon</h1>
+			<div class="attribution">
+				<p>
+					Challenge by
+					<a
+						href="https://www.frontendmentor.io?ref=challenge"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Frontend Mentor
+					</a>
+					and Coded by{" "}
+					<a href="https://github.com/bruno-gurgel" target="_blank" rel="noreferrer">
+						Bruno Gurgel
+					</a>
+					.
+				</p>
+			</div>
+		</div>
+	);
 }
-
-export default App;
