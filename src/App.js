cc929462b9c0f3cddc19e72e7f3c02aea8c696d9
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-04-16 17:53:39 -0300

    feat: Added basic dates to countdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
 
 export default function App() {
+	function milisecondsToCountdown(miliseconds) {
+		const days = Math.floor(miliseconds / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((miliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		const minutes = Math.floor((miliseconds % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((miliseconds % (1000 * 60)) / 1000);
+
+		return {
+			days,
+			hours,
+			minutes,
+			seconds,
+		};
+	}
+
+	const openingDate = new Date(Date.now() + 10 * 24 * 60 * 60 * 1000);
+	const today = new Date(Date.now());
+
+	const countdown = milisecondsToCountdown(new Date(openingDate - today));
+
 	return (
 		<div className="App">
 			<h1 className="heading">We're launching soon</h1>
+			<div>Days Hours Minute Seconds</div>
 			<div class="attribution">
 				<p>
 					Challenge by
