2aa695f7ab7d663a4ae2f7dfce94a63b780b7b75
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-04-17 12:23:54 -0300

    feat: Added basic timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import facebookIcon from "./assets/icon-facebook.svg";
 import instagramIcon from "./assets/icon-instagram.svg";
 import pinterestIcon from "./assets/icon-pinterest.svg";
+import NumberBoxWithLabel from "./components/NumberBoxWithLabel";
 
 export default function App() {
+	const [countdown, setCountDown] = useState(calculateCountdown());
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setCountDown(calculateCountdown());
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	});
+
+	// * TODO : FIX TIMER
+
+	function calculateCountdown() {
+		const year = new Date().getFullYear();
+		const month = new Date().getMonth();
+		const today = new Date(`${month}/10/${year}`);
+		const openingDate = new Date(`${month + 1}/10/${year}`);
+
+		const difference = new Date(openingDate - today);
+
+		const timeLeft = {
+			days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+			hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+			minutes: Math.floor((difference / 1000 / 60) % 60),
+			seconds: Math.floor((difference / 1000) % 60),
+		};
+
+		return timeLeft;
+	}
+
 	function milisecondsToCountdown(miliseconds) {
 		const days = Math.floor(miliseconds / (1000 * 60 * 60 * 24));
 		const hours = Math.floor((miliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -18,37 +50,40 @@ export default function App() {
 		};
 	}
 
-	const openingDate = new Date(Date.now() + 10 * 24 * 60 * 60 * 1000);
-	const today = new Date(Date.now());
-
-	const countdown = milisecondsToCountdown(new Date(openingDate - today));
-
 	return (
 		<div className="App">
 			<h1 className="heading">We're launching soon</h1>
-			<div>Days Hours Minute Seconds</div>
-			<div className="social-media">
-				<img src={facebookIcon} alt="Facebook icon" />
-				<img src={pinterestIcon} alt="Facebook icon" />
-				<img src={instagramIcon} alt="Facebook icon" />
+			<div className="countdown">
+				<NumberBoxWithLabel number={countdown.days} label="days" />
+				<NumberBoxWithLabel number={countdown.hours} label="hours" />
+				<NumberBoxWithLabel number={countdown.minutes} label="minutes" />
+				<NumberBoxWithLabel number={countdown.seconds} label="seconds" />
 			</div>
-			<div class="attribution">
-				<p>
-					Challenge by
-					<a
-						href="https://www.frontendmentor.io?ref=challenge"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Frontend Mentor
-					</a>
-					and Coded by{" "}
-					<a href="https://github.com/bruno-gurgel" target="_blank" rel="noreferrer">
-						Bruno Gurgel
-					</a>
-					.
-				</p>
-			</div>
+
+			<footer className="footer">
+				<div className="social-media">
+					<img src={facebookIcon} alt="Facebook icon" />
+					<img src={pinterestIcon} alt="Facebook icon" />
+					<img src={instagramIcon} alt="Facebook icon" />
+				</div>
+				<div class="attribution">
+					<p>
+						Challenge by
+						<a
+							href="https://www.frontendmentor.io?ref=challenge"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Frontend Mentor
+						</a>
+						and Coded by{" "}
+						<a href="https://github.com/bruno-gurgel" target="_blank" rel="noreferrer">
+							Bruno Gurgel
+						</a>
+						.
+					</p>
+				</div>
+			</footer>
 		</div>
 	);
 }
